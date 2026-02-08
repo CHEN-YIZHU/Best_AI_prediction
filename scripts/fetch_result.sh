7085d3adde8cea595f03fcebb2762990ff7fd3f6
yzchen <cs_yzchen@163.com>
2026-02-08 21:28:09 +0800

    Obscure OPENAI_API_KEY in fetch_result.sh

--- scripts/fetch_result.sh
+++ scripts/fetch_result.sh
@@ -1,6 +1,6 @@
 export AI_PROVIDER=qwen
 export AI_BASE_URL=https://dashscope.aliyuncs.com/compatible-mode/v1
-export OPENAI_API_KEY=sk-a5003d95f24b49ebb40c1927f126fba1
+export OPENAI_API_KEY=sk...
 export OPENAI_MODEL=qwen-max
 
 python api/api_example.py
